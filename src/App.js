005fcdfaa43b6dfd23f169b6d744bf865feb9186
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-09-24 15:57:03 +0100

    Add the Cart and Modal components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
     <div>
       <Header />
+      <Cart />
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
 
-const Cart = props => {
-const cartItem = [{}, {}, {}]
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 28.5 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
-return (
-  <div>
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>3</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
 
-  </div>
-)
-}
-
-export default Cart
+export default Cart;
