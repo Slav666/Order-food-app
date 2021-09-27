2749cec8fc94a1023a4d291df8bce0e21e85c165
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-09-27 09:54:15 +0100

    Managing cart and modal states

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 28.5 }].map((item) => (
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 28.5 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>3</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button onClick={props.onClose} className={classes["button--alt"]}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const BackDrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div onClick={props.onClose} className={classes.backdrop}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -18,7 +18,10 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<BackDrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <BackDrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
